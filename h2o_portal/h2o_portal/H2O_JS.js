5dcbe81fc00b89d5a53a650efe74f0a50449a6a9
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-11-02 21:50:38 -0400

    modified:   h2o_portal/H2O_Social_Tool.html 	modified:   h2o_portal/h2oStyles.css 	modified:   h2o_portal/h2o_portal/H2O_JS.js

--- h2o_portal/h2o_portal/H2O_JS.js
+++ h2o_portal/h2o_portal/H2O_JS.js
@@ -1,12 +1,22 @@
 // H2O Social Tool Methods
 //var isAuth = ('{{ user.isauthenticated}}');
-
-function RevealWindow(){
-    ButtonInput = document.getElementById(class=button);
-    if (ButtonInput) true;
-    document.getElementById("menuProfile").style.display="block";
-    document.getElementById("showProfile").style.display="none";
+function RevealWindow(){      
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    } 
 }
 
+// from the stacks
+// function toggle(button)
+// {
+//   if(document.getElementById("1").value=="OFF"){
+//    document.getElementById("1").value="ON";}
+
+//   else if(document.getElementById("1").value=="ON"){
+//    document.getElementById("1").value="OFF";}
+// }
 //state management and lazy loading
 //redux
