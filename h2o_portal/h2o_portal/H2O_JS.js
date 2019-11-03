af3db1848721527ebc388013cfe64d5dbe9c1dcd
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-11-02 20:33:44 -0400

    modified:   h2o_portal/H2O_Social_Tool.html 	modified:   h2o_portal/h2oStyles.css 	new file:   h2o_portal/h2o_portal/H2O_JS.js

---
+++ h2o_portal/h2o_portal/H2O_JS.js
@@ -0,0 +1,12 @@
+// H2O Social Tool Methods
+//var isAuth = ('{{ user.isauthenticated}}');
+
+function RevealWindow(){
+    ButtonInput = document.getElementById(class=button);
+    if (ButtonInput) true;
+    document.getElementById("menuProfile").style.display="block";
+    document.getElementById("showProfile").style.display="none";
+}
+
+//state management and lazy loading
+//redux
