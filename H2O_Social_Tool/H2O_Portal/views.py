4918daa9b0fe4cb2ae913874d59c8322cb5623f5
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-11-08 00:34:35 -0500

    Finally got the page statically hosted on Django

--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -3,8 +3,7 @@ from django.template import context, loader
 from django.http import HttpResponse
 
 def home(request):
-    context = {}
-    return render(request, 'H2O_Portal/base.html', context)
+    return render(request, 'H2O_Portal/base.html')
     #return render(request, 'H2O_Portal/Social_Tool.html')
 
 
--- H2O_Social_Tool/H2O_Social_Tool/settings.py
+++ H2O_Social_Tool/H2O_Social_Tool/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, '/H2O_portal/static')
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     'C:/Users/Nickolaus/Documents/GitHub/BaseRepoGitHub/H2O_Social_Tool/H2O_Portal/',
+# ]
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
