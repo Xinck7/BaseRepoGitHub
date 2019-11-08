5a277a072f04265791504166e7633e79f415ab8c
Nick <>
2019-11-08 13:30:28 -0500

    modified:   H2O_Portal/__pycache__/views.cpython-37.pyc 	new file:   H2O_Portal/migrations/0001_initial.py 	new file:   H2O_Portal/migrations/__pycache__/0001_initial.cpython-37.pyc 	modified:   H2O_Portal/views.py 	modified:   db.sqlite3 	modified:   ../requirements.txt

--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.template import context, loader
 
+# Create your views here.
+
 def home(request):
     return render(request, 'H2O_Portal/base.html')
-    #return render(request, 'H2O_Portal/Social_Tool.html')
 
 
-# Create your views here.
