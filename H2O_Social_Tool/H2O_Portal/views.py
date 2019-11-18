6c8a11768c858d305a9de0cfbfd8763439776aa3
Nick <>
2019-11-18 17:21:06 -0500

    commit view testing

--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -5,15 +5,9 @@ from H2O_Portal.models import *
 # Create your views here.
 
 def home(request):
-    return render(request, 'H2O_portal/base.html')
+    return render(request, 'base.html')
 
 def post(request):
     posts = Post.objects.all()
-    post_titles = list()
-
-    for post in posts:
-        post_titles.append(Post.title)
-    
-    response_html = '<br>'.join(post_titles)
-    return render(request, 'H2O_portal/SchPost.html')
+    return render(request, 'SchPost.html', {'Posts': posts})
 
