9d4136982fd184f9d9c1a6a953644700d48f9b3a
Nick <>
2019-11-19 12:12:15 -0500

    finally got the pieces moving :D

--- H2O_Social_Tool/H2O_Portal/urls.py
+++ H2O_Social_Tool/H2O_Portal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='H2OHome'),
-    path('Posts/', views.post, name='posts')
+    #path('Posts/', views.post, name='posts')
     #path('Credentials/', views.Credentials, name='StoredCredentials'),
     #path('Posts/', views.Post, name='Posts')
 ]
--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.template import context, loader
 from H2O_Portal.models import *
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'base.html')
+#def home(request):
+#    return render(request, 'base.html')
 
-def post(request):
-    posts = Post.objects.all()
-    return render(request, 'SchPost.html', {'Posts': posts})
+def home(request):
+    all_posts = Post.objects.all()
+    posts_titles = list()
+    for post in all_posts:
+        posts_titles.append(post.title)
+    response_html = '<br>'.join(posts_titles)
+    return HttpResponse(response_html) 
+    #return render(request, 'SchPost.html', {'Posts': posts})
+    #return render(request, 'base.html')
 
