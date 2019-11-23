0b8ed8d4b0877c216e651e01146cac354dbe54a6
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-11-23 00:12:47 -0500

    ok sign up form setup well enough - still not excited about the final color but not sure how to dial it back

--- H2O_Social_Tool/H2O_Portal/forms.py
+++ H2O_Social_Tool/H2O_Portal/forms.py
@@ -3,8 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Enter an email address.')
-
+    email = forms.EmailField(max_length=254)
     class Meta:
         model = User
+        widgets = {
+            'myfield': forms.TextInput(attrs={'class': 'myfieldclass'}),
+        }
         fields = ('username', 'email', 'password1', 'password2', )
+
