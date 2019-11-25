68a1445fb953e460c1d5cfdb0f6410ba0561e3de
Nick <>
2019-11-25 13:56:42 -0500

    maybe I didn't simplify it - tried to throw a break in there and it didn't return as a bad test - manual typing it is for now

--- H2O_Social_Tool/H2O_Portal/tests.py
+++ H2O_Social_Tool/H2O_Portal/tests.py
@@ -14,54 +14,55 @@ class TestUrls(TestCase):
         view = resolve('/')
         self.assertEquals(view.func, home)
     
-    def test_view_list(self):
-        array = ['signup','managecreds','listscheduled', 'listcompleted' ]
-        length = len(array)
-        for i in range(length):
-            def test_item_view_status_code(self):
-                url = reverse(array[i])
-                response = self.client.get(url)
-                self.assertEquals(response.status_code, 200)
+    # def test_view_list(self):
+    #     array = ['signup','managecreds','listscheduled', 'listcompleted', 'test' ]
+    #     length = len(array)
+    #     for i in range(length):
+    #         def test_item_view_status_code(self):
+    #             url = reverse(array[i])
+    #             response = self.client.get(url)
+    #             self.assertEquals(response.status_code, 200)
             
-            def test_item_url_resolves_item_view(self):
-                view = resolve('/'+ array[i] + '/')
-                self.assertEquals(view.func, array[i])            
-    # #Signup
-    # def test_signup_view_status_code(self):
-    #     url = reverse('signup')
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 200)
+    #         def test_item_url_resolves_item_view(self):
+    #             view = resolve('/'+ array[i] + '/')
+    #             self.assertEquals(view.func, array[i])
 
-    # def test_signup_url_resolves_signup_view(self):
-    #     view = resolve('/signup/')
-    #     self.assertEquals(view.func, signup)
+    #Signup
+    def test_signup_view_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
 
-    # #Managecredentials
-    # def test_managecreds_view_status_code(self):
-    #     url = reverse('managecreds')
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 200)
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
 
-    # def test_managecreds_url_resolves_managecreds_view(self):
-    #     view = resolve('/managecreds/')
-    #     self.assertEquals(view.func, managecreds)
+    #Managecredentials
+    def test_managecreds_view_status_code(self):
+        url = reverse('managecreds')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
 
-    # #List scheduled
-    # def test_listscheduled_view_status_code(self):
-    #     url = reverse('listscheduled')
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 200)
+    def test_managecreds_url_resolves_managecreds_view(self):
+        view = resolve('/managecreds/')
+        self.assertEquals(view.func, managecreds)
 
-    # def test_listscheduled_url_resolves_listscheduled_view(self):
-    #     view = resolve('/listscheduled/')
-    #     self.assertEquals(view.func, listscheduled)
+    #List scheduled
+    def test_listscheduled_view_status_code(self):
+        url = reverse('listscheduled')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
 
-    # #List completed
-    # def test_listcompleted_view_status_code(self):
-    #     url = reverse('listcompleted')
-    #     response = self.client.get(url)
-    #     self.assertEquals(response.status_code, 200)
+    def test_listscheduled_url_resolves_listscheduled_view(self):
+        view = resolve('/listscheduled/')
+        self.assertEquals(view.func, listscheduled)
 
-    # def test_listcompleted_url_resolves_listcompleted_view(self):
-    #     view = resolve('/listcompleted/')
-    #     self.assertEquals(view.func, listcompleted)
+    #List completed
+    def test_listcompleted_view_status_code(self):
+        url = reverse('listcompleted')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_listcompleted_url_resolves_listcompleted_view(self):
+        view = resolve('/listcompleted/')
+        self.assertEquals(view.func, listcompleted)
