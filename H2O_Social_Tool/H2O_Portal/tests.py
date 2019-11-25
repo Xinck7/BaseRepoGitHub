7bb0618a018662f446b43be53145e9a7cfcd4b40
Nick <>
2019-11-25 10:05:08 -0500

    saving prog on tests updates

--- H2O_Social_Tool/H2O_Portal/tests.py
+++ H2O_Social_Tool/H2O_Portal/tests.py
@@ -4,6 +4,7 @@ from .views import home
 
 # Create your tests here.
 class TestUrls(TestCase):
+    #home url
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -12,3 +13,53 @@ class TestUrls(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    #Signup
+    def test_signup_view_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('signup/')
+        self.assertEquals(view.func, signup)
+
+    #Login
+    def test_loginuser_view_status_code(self):
+        url = reverse('loginuser')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_loginuser_url_resolves_loginuser_view(self):
+        view = resolve('loginuser/')
+        self.assertEquals(view.func, loginuser)
+
+    #Managecredentials
+    def test_managecreds_view_status_code(self):
+        url = reverse('managecreds')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_managecreds_url_resolves_managecreds_view(self):
+        view = resolve('managecreds/')
+        self.assertEquals(view.func, managecreds)
+
+    #List scheduled
+    def test_listscheduled_view_status_code(self):
+        url = reverse('listscheduled')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_listscheduled_url_resolves_listscheduled_view(self):
+        view = resolve('listscheduled/')
+        self.assertEquals(view.func, listscheduled)
+
+    #List completed
+    def test_listcompleted_view_status_code(self):
+        url = reverse('listcompleted')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_listcompleted_url_resolves_listcompleted_view(self):
+        view = resolve('listcompleted/')
+        self.assertEquals(view.func, listcompleted)
--- H2O_Social_Tool/H2O_Portal/urls.py
+++ H2O_Social_Tool/H2O_Portal/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.loginuser, name='loginuser' ),
+    path('login/', views.loginuser, name='loginuser'),
     path('managecreds/', views.managecreds, name='managecreds'),
     path('createpost/', views.createpost, name='createpost'),
     path('listscheduled/', views.listscheduled, name='listscheduled'),
