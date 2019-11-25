cc46126b0adfaceca45ce4ccb52912c130791a10
Nick <>
2019-11-25 10:13:12 -0500

    signup officially works on new format

--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -16,7 +16,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             auth_login(request, user)
-            return redirect('')
+            return redirect('/')
     else:
         form = SignUpForm()
 
