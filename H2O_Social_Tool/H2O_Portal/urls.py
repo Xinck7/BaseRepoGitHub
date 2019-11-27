4b0244ae9a630299a15597ef6d4a4366b1ce65bb
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-11-27 00:15:39 -0500

    form shows up on the update now I need the method to save it

--- H2O_Social_Tool/H2O_Portal/urls.py
+++ H2O_Social_Tool/H2O_Portal/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('managecreds/', views.managecreds, name='managecreds'),
     path('createpost/', views.createpost, name='createpost'),
     path('listscheduled/', views.listscheduled, name='listscheduled'),
-    #path('editpost/', views.editpost, name='editpost'),
     path('editpost/editpost<int:value>', views.editpost, name='editpost'),
     path('editpost/deletepost<int:value>', views.deletepost, name='deletepost'),
     path('listcompleted/', views.listcompleted, name='listcompleted'),
--- H2O_Social_Tool/H2O_Portal/views.py
+++ H2O_Social_Tool/H2O_Portal/views.py
@@ -61,7 +61,15 @@ def editpost(request, value):
     #need title drop down
     user = request.user
     user_posts = SocialPost.objects.filter(updated_by=user, id=value)
-    return render(request, 'H2O_Portal/editpost.html', {'Posts' : user_posts})
+    if request.method == 'POST':
+        form = SocialPostForm(request.POST)            
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('H2O_Portal/listscheduled.html')
+    else:
+        form = SocialPostForm()
+    return render(request, 'H2O_Portal/editpost.html', {'Posts' : user_posts, 'form' : form} )
 
 @login_required
 def deletepost(request, value):
@@ -69,7 +77,8 @@ def deletepost(request, value):
     user = request.user
     user_post = SocialPost.objects.filter(updated_by=user, id=value)
     user_post.delete()
-    return render(request, 'H2O_Portal/editpost.html')
+    all_posts = SocialPost.objects.all()
+    return render(request, 'H2O_Portal/listscheduled.html' , {'Posts': all_posts })
 
 @login_required
 def listcompleted(request):
