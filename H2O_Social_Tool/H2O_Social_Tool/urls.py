18db320a7a4a57e6026fc3edc9189e97107e51e9
Nick <nickandliz@Nicks-MacBook-Pro-3.local>
2019-12-11 22:01:00 -0500

    commented things not working within venv for prod

--- H2O_Social_Tool/H2O_Social_Tool/urls.py
+++ H2O_Social_Tool/H2O_Social_Tool/urls.py
@@ -21,7 +21,7 @@ admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('H2O_Portal/', include('django.contrib.auth.urls')),
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    #url(r'^oauth/', include('social_django.urls', namespace='social')),
     path('accounts/', include('allauth.urls')),
     path('', include('H2O_Portal.urls')),
 ]
