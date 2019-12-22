fb66e9daecbd6942ad1299f4e7b815643e943bf0
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-12-21 22:19:24 -0500

    trying mod on staticfiles

--- H2O_Social_Tool/H2O_Social_Tool/settings.py
+++ H2O_Social_Tool/H2O_Social_Tool/settings.py
@@ -140,10 +140,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Login URL's
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
