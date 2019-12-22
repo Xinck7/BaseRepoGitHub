d46f9313e8b37f33fce83a38a926aa1b061de6d7
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-12-21 21:51:15 -0500

    changing settings to reflect staticroot

--- H2O_Social_Tool/H2O_Social_Tool/settings.py
+++ H2O_Social_Tool/H2O_Social_Tool/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= (os.path.dirname(BASE_DIR), 'staticfiles')
 # Login URL's
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
