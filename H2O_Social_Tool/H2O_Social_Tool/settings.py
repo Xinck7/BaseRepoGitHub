10b16fdf8ccf0e4f972032371c9bdd4492ff9510
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-12-21 21:52:43 -0500

    once more with feeling on staticfiles

--- H2O_Social_Tool/H2O_Social_Tool/settings.py
+++ H2O_Social_Tool/H2O_Social_Tool/settings.py
@@ -141,6 +141,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= (os.path.dirname(BASE_DIR), 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Login URL's
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
