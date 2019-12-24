4d5bc7866af5f9c92dfdd72ddb2e2c2bdab4b609
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-12-23 19:46:20 -0500

    again same thing as last commit

--- H2O_Social_Tool/H2O_Social_Tool/settings.py
+++ H2O_Social_Tool/H2O_Social_Tool/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/H2O_Portal/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
