2e93ead927c1c0109fbb6cc48d4d1e17e40dfd2c
Nickolaus V <56853413+Xinck7@users.noreply.github.com>
2019-12-31 00:54:10 -0500

    adding time comparison to the management command itself instead of the model

--- H2O_Social_Tool/H2O_Portal/management/commands/sendposts.py
+++ H2O_Social_Tool/H2O_Portal/management/commands/sendposts.py
@@ -7,20 +7,21 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         unposted = SocialPost.objects.filter(completed=False)
         all_users = User.objects.all()
+        utc=pytz.UTC
         for post in unposted:
-            if (post.GroupMe == True) or (post.GroupMeGroups != None):
-                groupnames = post.GroupMeGroups
-                user_name = post.updated_by
-                filter_user = all_users.filter(username=user_name)
-                user_db_info = filter_user.values('gm_auth_token')
-                token_dict = dict()
-                token_dict = user_db_info.get()
-                auth_token = token_dict['gm_auth_token']
-                groupnames = post.GroupMeGroups
-                if auth_token != None:
-                    gmp = GroupMePosts()
-                    gmp.sendmessages(auth_token, groupnames, post)
-                    post.completed = True
+            if post.post_time <= utc.localize(datetime.datetime.now()):
+                if (post.GroupMe == True) or (post.GroupMeGroups != None):
+                    groupnames = post.GroupMeGroups
+                    user_name = post.updated_by
+                    filter_user = all_users.filter(username=user_name)
+                    user_db_info = filter_user.values('gm_auth_token')
+                    token_dict = dict()
+                    token_dict = user_db_info.get()
+                    auth_token = token_dict['gm_auth_token']
+                    if auth_token != None:
+                        gmp = GroupMePosts()
+                        gmp.sendmessages(auth_token, groupnames, post)
+                        post.completed = True
 
             # if post.Facebook == True:
             #     init_posts = FacebookPosts()
